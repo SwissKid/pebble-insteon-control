520fa51e7c4279bc30f8d40604ed58a54f5c3dd0
Chris Schafer <cpschafer@csupomona.edu>
2015-06-29 22:01:33 -0700

    No real reason to hide the API key
    it gets called in the published app... and I'm no longer hiding secrets in all of this.

--- src/secrets.js
+++ src/secrets.js
@@ -1,6 +1,5 @@
-//To use this, you just put your api key in, and you'll have to configure your own url. Included go server as well.
-var secrets = {
-  API_Key : "example_api_key_134fhn80w35hynainrsg0q824hyn",
-  insteon_server : "https://private-anon-a17c67615-insteon.apiary-mock.com"
+ var secrets = { 
+  API_Key : "1ed0fedf-8792-40ba-a500-f34a02d17d2c1422000359.6140731",
+  insteon_server : "https://connect.insteon.com"
 };
 this.exports = secrets;
